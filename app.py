e0cf2af6072f30a46cac99683deac14b09fe9d3d
Oppu-king <monjitkurmi96@gmail.com>
2025-09-10 13:07:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session
 from flask_cors import CORS
 import os
 import requests
